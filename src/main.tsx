371231c5e80d028d34cc02056437b505bff45300
laeticiamng <m.laeticia@hotmail.fr>
2025-05-18 15:49:19 +0200

    feat: add Sentry monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './monitoring';
+import * as Sentry from '@sentry/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Sentry.ErrorBoundary fallback={<p>Une erreur est survenue.</p>} showDialog>
+      <App />
+    </Sentry.ErrorBoundary>
   </React.StrictMode>
 );
